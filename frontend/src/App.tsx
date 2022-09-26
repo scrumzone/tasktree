b48b8a340d066dc1412db30e349c4ec9fed729f6
Andrew Rutter <andlrutt@vols.utk.edu>
2022-09-25 23:33:53 -0400

    routing and BASIC nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import logo from './assets/logo.png';
 import LoginPage from './pages/login';
 import HomePage from './pages/home';
 import SignupPage from './pages/signup';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -14,6 +14,14 @@ function App() {
     return (
         <div className="App">
             <Router>
+            <nav>
+                <ul className="nav">
+                    <li><NavLink to="/login">Log in</NavLink></li>
+                    <li><NavLink to="/signup">Sign up</NavLink></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                </ul>
+            </nav>
+            <br />
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<LoginPage />} />
