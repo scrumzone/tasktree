a4447f77aa71b34fb33f8ce51ddcf7154855c42f
Zavier Miller <zavierjmiller@gmail.com>
2022-09-29 20:48:11 -0400

    set up mui

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { Button, Typography } from '@mui/material';
 
-class HomePage extends React.Component
-{
-    render (){
-        return (
-            <div>
-                HOME PAGE
-            </div>
-        )
-    }
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Typography variant="h1">HOME PAGE</Typography>
+        <Button>Hello World</Button>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
