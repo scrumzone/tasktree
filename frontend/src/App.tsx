81c81575dcf78532e96b0ac6fdddce22b3418d3d
Zavier Miller <zavier@resaleai.com>
2022-09-29 22:28:29 -0400

    implement nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,32 +4,48 @@ import logo from './assets/logo.png';
 import LoginPage from './pages/login';
 import HomePage from './pages/home';
 import SignupPage from './pages/signup';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from 'react-router-dom';
 import './App.css';
+import TTNavBar from './components/TTNavBar/index';
 
 function App() {
-    useEffect(() => {
-        document.title = 'Tasktree';
-    });
-    return (
-        <div className="App">
-            <Router>
-            <nav>
-                <ul className="nav">
-                    <li><NavLink to="/login">Log in</NavLink></li>
-                    <li><NavLink to="/signup">Sign up</NavLink></li>
-                    <li><NavLink to="/">Home</NavLink></li>
-                </ul>
-            </nav>
-            <br />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignupPage />} />
-                </Routes>
-            </Router>
-        </div>
-    );
+  useEffect(() => {
+    document.title = 'Tasktree';
+  });
+
+  const navItems = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'Login',
+      path: '/login',
+    },
+    {
+      name: 'Signup',
+      path: '/signup',
+    },
+  ];
+
+  return (
+    <div className="App">
+      <Router>
+        <TTNavBar navItems={navItems} />
+        <br />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
