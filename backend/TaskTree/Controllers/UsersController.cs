5da555a6efe47387033d359b48d2d598cacc8a48
Zavier Miller <zavierjmiller@gmail.com>
2022-09-30 09:14:36 -0400

    fix pr changes

--- backend/TaskTree/Controllers/UsersController.cs
+++ backend/TaskTree/Controllers/UsersController.cs
@@ -7,120 +7,127 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TaskTree.Models;
+using TaskTree.Models.Requests;
 
 namespace TaskTree.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase
+  {
+    private readonly TaskTreeContext _context;
+    private readonly IMapper _mapper;
+
+    public UsersController(TaskTreeContext context, IMapper mapper)
     {
-        private readonly TaskTreeContext _context;
-        private readonly IMapper _mapper;
-
-        public UsersController(TaskTreeContext context, IMapper mapper)
-        {
-            _context = context;
-            _mapper = mapper;
-        }
-
-
-        // GET: api/users/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(long id)
-        {
-          if (_context.Users == null)
-          {
-              return NotFound();
-          }
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return user;
-        }
-
-        // PUT: api/users/5
-        // TODO: ensure only authorized user can update their account
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateUser(long id, Models.Requests.UpdateUserRequest updateUserRequest)
-        {
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-            
-            // map using automapper
-            _mapper.Map(updateUserRequest, user);
-
-            _context.Entry(user).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/users
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<User>> CreateUser(Models.Requests.CreateUserRequest createUserRequest)
-        {
-            if (_context.Users == null)
-            {
-                return Problem("Entity set 'TaskTreeContext.Users'  is null.");
-            }
-
-            var user = _mapper.Map<User>(createUserRequest);
-            
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
-        }
-
-        // DELETE: api/Users/5
-        // TODO: ensure only authorized user can delete their account
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(long id)
-        {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool UserExists(long id)
-        {
-            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+      _context = context;
+      _mapper = mapper;
     }
+
+
+    // GET: api/users/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(long id)
+    {
+      if (_context.Users == null)
+      {
+        return Problem("Entity set 'TaskTreeContext.Users'  is null.", statusCode: 500);
+      }
+      var user = await _context.Users.FindAsync(id);
+
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      return user;
+    }
+
+    // PUT: api/users/5
+    // TODO: ensure only authorized user can update their account
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(long id, UpdateUserRequest updateUserRequest)
+    {
+      
+      if (_context.Users == null)
+      {
+        return Problem("Entity set 'TaskTreeContext.Users'  is null.", statusCode: 500);
+      }
+      
+      var user = await _context.Users.FindAsync(id);
+
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      // map using automapper
+      _mapper.Map(updateUserRequest, user);
+
+      _context.Entry(user).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!UserExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/users
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser(CreateUserRequest createUserRequest)
+    {
+      if (_context.Users == null)
+      {
+        return Problem("Entity set 'TaskTreeContext.Users'  is null.", statusCode: 500);
+      }
+
+      var user = _mapper.Map<User>(createUserRequest);
+
+      _context.Users.Add(user);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+    }
+
+    // DELETE: api/Users/5
+    // TODO: ensure only authorized user can delete their account
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(long id)
+    {
+      if (_context.Users == null)
+      {
+        return Problem("Entity set 'TaskTreeContext.Users'  is null.", statusCode: 500);
+      }
+      
+      var user = await _context.Users.FindAsync(id);
+      if (user == null)
+      {
+        return NotFound();
+      }
+
+      _context.Users.Remove(user);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool UserExists(long id)
+    {
+      return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+    }
+
+  }
 }
