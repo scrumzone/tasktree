17f11d8bb4b4c0308249be456489c263a7acd526
ethanhmaness <emaness@vols.utk.edu>
2022-10-04 12:46:28 -0400

    schema

---
+++ backend/TaskTree/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskTree.Models
+{
+	[Index(nameof(Name), IsUnique = true)]
+	public class Task : BaseEntity
+	{
+		[Required]
+		public string Name { get; set; }
+		public string? Description { get; set; }
+		public double? Progress { get; set; }
+		public List<Task>? Children { get; set; }
+	}
+}
