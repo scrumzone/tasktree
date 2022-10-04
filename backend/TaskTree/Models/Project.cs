bf72d0597e0a8da67caa944488b6a38f65d17da8
ethanhmaness <emaness@vols.utk.edu>
2022-10-04 12:56:28 -0400

    task and project schema

---
+++ backend/TaskTree/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskTree.Models
+{
+	[Index(nameof(Id), IsUnique = true)]
+	public class Project : BaseEntity
+	{
+		[Required]
+		public int Name { get; set; }
+		public string? Description { get; set; }
+		public Task? Root { get; set; }
+		public double? Progress	{ get; set; }
+	}
+}
