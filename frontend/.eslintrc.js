20af5fa530b35bbaa784a9d1dcf50460fae1e1ee
Zavier Miller <zavierjmiller@gmail.com>
2022-10-10 20:57:08 -0400

    finish up linting

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,16 +2,21 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    settings: {
+      react: {
+        version: '18.2.0'
+      }
+    }
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-  },
+    'react/react-in-jsx-scope': 'off'
+  }
 };
