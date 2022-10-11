a26825132066229703755745cbafe07c2cd4894a
PotassiumLover <107872773+PotassiumLover@users.noreply.github.com>
2022-10-10 21:56:46 -0400

    Update authService.ts

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,7 +1,7 @@
 import {setCookie, removeCookie} from 'typescript-cookie';
 
 export default class AuthService{
-    static async storeJWT(jwt){
+    static async storeJWT(jwt: string){
         setCookie("access_token", jwt);
     }
 
