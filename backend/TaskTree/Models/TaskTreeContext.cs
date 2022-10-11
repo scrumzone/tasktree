5bacc00c1d19d727eb5c29683233c76625493e1f
Zavier Miller <zavierjmiller@gmail.com>
2022-10-10 21:15:49 -0400

    fix TaskTreeContext.cs

--- backend/TaskTree/Models/TaskTreeContext.cs
+++ backend/TaskTree/Models/TaskTreeContext.cs
@@ -1,7 +1,7 @@
-﻿namespace TaskTree.Models;
-
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+
+namespace TaskTree.Models;
 
 public class TaskTreeContext : DbContext
 {
@@ -50,28 +50,6 @@ public class TaskTreeContext : DbContext
 
         foreach (var entry in entries)
         {
-        }
-
-    public override int SaveChanges(bool acceptChangesOnSuccess)
-    {
-        OnBeforeSave();
-        return base.SaveChanges(acceptChangesOnSuccess);
-    }
-
-    public override async Task<int> SaveChangesAsync(bool acceptChangesOnSuccess,
-      CancellationToken cancellationToken = default(CancellationToken))
-    {
-        OnBeforeSave();
-        return (await base.SaveChangesAsync(acceptChangesOnSuccess, cancellationToken));
-    }
-
-    private void OnBeforeSave()
-    {
-        var entries = ChangeTracker.Entries();
-        var currentDateTime = DateTime.UtcNow;
-
-        foreach (var entry in entries)
-        {
             if (entry.Entity is BaseEntity trackable)
             {
                 switch (entry.State)
@@ -91,5 +69,6 @@ public class TaskTreeContext : DbContext
 
     // add DbSets here
     public DbSet<User> Users { get; set; }
-
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<Project> Projects { get; set; }
 }
