3490f90c88e0bbd1ed0f16497f8723e32f8e0841
Andrew Rutter <andlrutt@vols.utk.edu>
2022-10-10 23:50:49 -0400

    implementation

--- frontend/src/components/Login/LoginDesktop.tsx
+++ frontend/src/components/Login/LoginDesktop.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import './login.css';
+import UserService from '../../services/UserService';
+import AuthService from '../../services/authService';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -13,15 +15,26 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
+
+
 export default function LoginDesktop() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onSubmit = async () => {
+        const jwt: string = await UserService.authenticateUser(username, password);
+        AuthService.storeJWT(jwt);
+    }
+
     return (
         <div id="wrapper1">
             <div id="loginField">
                 <h1>Account Log In</h1>
                 <form>
-                <TextField id="userIn" variant="outlined" placeholder='Username'></TextField><br/><br/>
-                <TextField id="passIn" variant="outlined" placeholder='Password'></TextField>
-                <Button id="loginBtn" variant="contained">Log In</Button>
+                    <TextField id="userIn" onChange={(e) => setUsername(e.target.value)} variant="outlined" placeholder='Username'></TextField><br/><br/>
+                    <TextField id="passIn" onChange={(e) => setPassword(e.target.value)} variant="outlined" placeholder='Password'></TextField>
+                    <Button id="loginBtn" onClick={onSubmit} variant="contained">Log In</Button>
                 </form>
             </div>
         </div>
