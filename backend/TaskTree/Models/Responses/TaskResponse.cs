255f053f8aa076970102dc69eda44e42f3160830
emaness-utk <emaness@vols.utk.edu>
2022-10-18 08:38:52 -0400

    added response objects

---
+++ backend/TaskTree/Models/Responses/TaskResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskTree.Models.Responses;
+
+public class TaskResponse
+{
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public double? Progress { get; set; }
+    public double? Weight { get; set; }
+    public DateTime? CompletedAt { get; set; }
+    public List<TaskResponse>? Children { get; set; }
+}
