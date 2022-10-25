699f01372b9d0cbb1a27671fe6c51cd9130f9f73
Zavier Miller <zavierjmiller@gmail.com>
2022-10-25 01:01:30 -0400

    rename

---
+++ frontend/src/pages/projects.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import DisplayProjects from '../components/DisplayProjects';
+
+export default function ProjectsPage() {
+  return React.createElement(DisplayProjects);
+}
