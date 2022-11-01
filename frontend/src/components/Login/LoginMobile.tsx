e9dedf8b77191c793a1f3309a5d103844b6da8ba
zmalkmus <zmalkmus@vols.utk.edu>
2022-11-01 09:24:12 -0400

    Updated LoginMobile component to reflect changes

--- frontend/src/components/Login/LoginMobile.tsx
+++ frontend/src/components/Login/LoginMobile.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import { TextField, Button, Link } from '@mui/material';
 import './login.css';
 import AuthService from '../../services/AuthService';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
@@ -9,41 +8,49 @@ import { useNavigate } from 'react-router-dom';
 import UserService from '../../services/UserService';
 
 export default function LoginMobile() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    const dispatch = useAppDispatch();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
-    const onSubmit = async () => {
-        const token = await UserService.authenticateUser(username, password);
-        const user = AuthService.decodeJWT(token);
-        dispatch(setCurrentUser(user));
-        navigate('/');
-    };
+  const onSubmit = async () => {
+    const token = await UserService.authenticateUser(username, password);
+    AuthService.storeJWT(token);
+    const user = AuthService.decodeJWT(token);
+    dispatch(setCurrentUser(user));
+    navigate('/');
+  };
 
-    return (
-        <div id="wrapper1">
-            <div id="loginField">
-                <h1>Account Log In</h1>
-                <form>
-                    <TextField
-                        id="userIn"
-                        onChange={(e) => setUsername(e.target.value)}
-                        variant="outlined"
-                        placeholder="Username"></TextField>
-                    <br />
-                    <br />
-                    <TextField
-                        id="passIn"
-                        onChange={(e) => setPassword(e.target.value)}
-                        variant="outlined"
-                        type="password"
-                        placeholder="Password"></TextField>
-                    <Button id="loginBtn" onClick={onSubmit} variant="contained">
-                        Log In
-                    </Button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div id="wrapper1">
+      <div id="loginField">
+        <h1>Account Log In</h1>
+        <form>
+          <TextField
+            id="userIn"
+            onChange={(e) => setUsername(e.target.value)}
+            variant="outlined"
+            placeholder="Username"></TextField>
+          <br />
+          <br />
+          <TextField
+            id="passIn"
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+            type="password" 
+            placeholder="Password"></TextField>
+          <Button id="loginBtn" onClick={onSubmit} variant="contained">
+            Log In
+          </Button><br></br><br></br>
+          
+          <div id="signUpDiv">
+            Don't have an account? &nbsp;
+            <Link className="existingUser" href="/SignUp" variant="body2">
+                Sign Up
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
