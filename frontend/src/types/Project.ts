34a971e83b9733c9bb377a44b964e4894cd8f250
zmalkmus <zmalkmus@vols.utk.edu>
2022-11-01 08:16:54 -0400

    Caught the id in the frontend types

--- frontend/src/types/Project.ts
+++ frontend/src/types/Project.ts
@@ -1,6 +1,7 @@
 import Task from './Task';
 
 export default interface Project {
+  id: number;
   name: string;
   description?: string;
   root?: Task;
@@ -8,6 +9,7 @@ export default interface Project {
 }
 
 export const BlankProject: Project = {
+  id: 0,
   name: 'blank',
   description: 'blank',
   progress: 0
