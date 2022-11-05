85d6c88650841e120d083b898e016b63094f92ad
emaness-utk <emaness@vols.utk.edu>
2022-11-05 15:07:46 -0400

    first pass

--- backend/TaskTree/Models/Task.cs
+++ backend/TaskTree/Models/Task.cs
@@ -17,5 +17,31 @@ namespace TaskTree.Models
         public Task? Parent { get; set; }
         public List<Task>? Children { get; set; }
         public long UserId { get; set; }
+
+        /// <summary>
+        /// Performs a breadth-first search on <see cref="Children"/> to find every <see cref="Task"/> descended from the caller.
+        /// </summary>
+        /// <returns>A <see cref="List{T}"/> containing every <see cref="Task"/> descended from the caller.</returns>
+        public List<Task> Descendents()
+        {
+            if (Children == null) return new List<Task>();
+
+            List<Task> unprocessedTasks = new List<Task>();
+            List<Task> processedTasks = new List<Task>();
+
+            unprocessedTasks.AddRange(Children);
+
+            Task t;
+
+            while (unprocessedTasks.Count != 0)
+            {
+                t = unprocessedTasks.ElementAt(0);
+                if (t.Children != null) unprocessedTasks.AddRange(t.Children);
+                unprocessedTasks.RemoveAt(0);
+                processedTasks.Add(t);
+            }
+
+            return processedTasks;
+        }
     }
 }
