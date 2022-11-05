76f02269433b223187b444be7061dec0d2c65014
Andrew Rutter <andlrutt@vols.utk.edu>
2022-11-04 23:46:18 -0400

    created TaskService.ts

---
+++ frontend/src/services/TaskService.ts
@@ -0,0 +1,39 @@
+import http, { authHeader } from '../util/http';
+import AuthService from './AuthService';
+import Task from '../types/Task';
+
+export default class TaskService {
+  static async getTask(id: number): Promise<Task> {
+    const response = await http.get(`/tasks/${id}`, {
+      headers: authHeader(AuthService.getJWT())
+    });
+    return response.data;
+  }
+
+  static async getTasks(taskId: number): Promise<Task[]> {
+    const response = await http.get(`/tasks?taskId=${taskId}`, {
+      headers: authHeader(AuthService.getJWT())
+    });
+    return response.data;
+  }
+
+  static async createTask(task: Task, parentId: number): Promise<any> {
+    const response = await http.post(`/tasks/${parentId}/create`, task, {
+      headers: authHeader(AuthService.getJWT())
+    });
+    return response.data;
+  }
+
+  static async updateTask(task: Task, id: number): Promise<Task> {
+    const response = await http.put(`/tasks/${id}`, task, {
+      headers: authHeader(AuthService.getJWT())
+    });
+    return response.data;
+  }
+
+  static async deleteTask(id: number) {
+    await http.delete(`/tasks/${id}`, {
+      headers: authHeader(AuthService.getJWT())
+    });
+  }
+}
