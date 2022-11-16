7dbac0e2a7bfb2bb4d5869bb3df3e5eee1212abc
Andrew Rutter <andlrutt@vols.utk.edu>
2022-11-15 22:02:43 -0500

    made BlankProject truly blank

--- frontend/src/components/ProjectHeader/index.tsx
+++ frontend/src/components/ProjectHeader/index.tsx
@@ -1,54 +1,54 @@
 import * as React from 'react';
-import DeleteIcon from '@mui/icons-material/Delete';
-import IconButton from '@mui/material/IconButton';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import ListItemButton from '@mui/material/ListItemButton';
 import Typography from '@mui/material/Typography';
-import Project from '../../types/Project';
-import ListItemText from '@mui/material/ListItemText';
-import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
-import { Alert, Box, ListItem, Snackbar } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
-import EditTaskDialog from '../EditTaskDialog';
+import Project, { BlankProject } from '../../types/Project';
+import { Skeleton } from '@mui/material';
 
 interface GetProjectFormProps {
   project: Project;
 }
 
 export default function ProjectHeader(props: GetProjectFormProps) {
-
   return (
-    <Typography component="h3" variant="h3"
+    <Typography
+      component="h3"
+      variant="h3"
       sx={{
         marginTop: 8,
         display: 'flex',
         flexDirection: 'row',
-        width: '100%',
+        width: '100%'
       }}>
-        <Typography align='left'
-          component="h3"
-          variant="h3"
-          sx={{
-            justifySelf: 'left',
-            marginRight: 'auto',
-            marginLeft: '30px'
-          }}>
-          {props.project.name}
-        </Typography>
-
-        <Typography align='center'
-          component="h3"
-          variant="h3"
-          sx={{
-            justifySelf: 'center',
-            
-            marginRight: 'auto'
-          }}>
-          {Math.round(props.project.progress)}% completed
-        </Typography>
+      <Typography
+        align="left"
+        component="h3"
+        variant="h3"
+        sx={{
+          justifySelf: 'left',
+          marginRight: 'auto',
+          marginLeft: '30px'
+        }}>
+        {props.project !== BlankProject ? (
+          props.project.name
+        ) : (
+          <Skeleton variant="rounded" sx={{ width: 300, height: 56 }} />
+        )}
       </Typography>
+
+      <Typography
+        align="center"
+        component="h3"
+        variant="h3"
+        sx={{
+          justifySelf: 'center',
+
+          marginRight: 'auto'
+        }}>
+        {props.project !== BlankProject ? (
+          `${Math.round(props.project.progress)}% completed`
+        ) : (
+          <Skeleton variant="rounded" sx={{ width: 300, height: 56 }} />
+        )}
+      </Typography>
+    </Typography>
   );
 }
