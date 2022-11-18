c8d5e0ec536450b0de57e03c3605de7d49697049
Andrew Rutter <andlrutt@vols.utk.edu>
2022-11-18 01:38:48 -0500

    added skeleton for text while loading

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Skeleton, Typography } from '@mui/material';
 import { BlankUser } from '../types/User';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import Project from '../types/Project';
+import Project, { BlankProject } from '../types/Project';
 import ProjectService from '../services/ProjectService';
 import ProjectList from '../components/ProjectList';
 
@@ -17,14 +17,27 @@ function getOpenProjects(projects: Project[]) {
 }
 
 function displayProjects(projects: Project[], setProjects: (e: Project[]) => void) {
+  /* projects are still loading */
+  if (projects[0] === BlankProject) {
+    return (
+      <Skeleton
+        variant="rounded"
+        sx={{ width: '500px', height: '42px', margin: 'auto', marginTop: '10px' }}
+      />
+    );
+  }
+
+  /* no projects */
   if (projects.length === 0) {
     return <Typography variant="h4">You have no projects.</Typography>;
   }
 
+  /* no open projects */
   if (getOpenProjects(projects).length === 0) {
     return <Typography variant="h4">You have no open projects.</Typography>;
   }
 
+  /* some open projects */
   return (
     <>
       <Typography variant="h4">
@@ -45,7 +58,7 @@ function displayProjects(projects: Project[], setProjects: (e: Project[]) => voi
 
 export default function HomePage() {
   const [user, setUser] = React.useState(BlankUser);
-  const [projects, setProjects] = React.useState<Project[]>([]);
+  const [projects, setProjects] = React.useState<Project[]>([BlankProject]);
   const dispatch = useAppDispatch();
 
   const current = useAppSelector((state) => state.user.current);
