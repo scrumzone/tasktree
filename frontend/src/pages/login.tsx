14ae925b38913ae436ef2417c5473b043ccf7dd1
zmalkmus <zmalkmus@vols.utk.edu>
2022-11-22 10:47:03 -0500

    updated submitting and formatted code

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { TextField, Button, Link, Typography } from '@mui/material';
-import AuthService from '../../services/AuthService';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { setCurrentUser } from '../../store/user';
+import AuthService from '../services/AuthService';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { setCurrentUser } from '../store/user';
 import { useNavigate } from 'react-router-dom';
-import UserService from '../../services/UserService';
+import UserService from '../services/UserService';
 
 export default function LoginDesktop() {
   const [username, setUsername] = useState('');
@@ -12,7 +12,8 @@ export default function LoginDesktop() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const onSubmit = async () => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const token = await UserService.authenticateUser(username, password);
     AuthService.storeJWT(token);
     const user = AuthService.decodeJWT(token);
@@ -21,22 +22,26 @@ export default function LoginDesktop() {
   };
 
   return (
-    <Typography sx={{marginTop:6}}>
-      <Typography component="h1" variant="h5">Account Log In</Typography><br/>
-      <form>
+    <Typography sx={{ marginTop: 6 }}>
+      <Typography component="h1" variant="h5">
+        Account Log In
+      </Typography>
+      <br />
+      <form onSubmit={handleSubmit}>
         <TextField
           id="userIn"
           onChange={(e) => setUsername(e.target.value)}
           variant="outlined"
           placeholder="Username"
+          required
           sx={{
-            width:500,
-            height:15,
-            marginBottom:5,
-          }
-        }/>
-        <br/>
-        <br/>
+            width: 500,
+            height: 15,
+            marginBottom: 5
+          }}
+        />
+        <br />
+        <br />
 
         <TextField
           id="passIn"
@@ -44,26 +49,29 @@ export default function LoginDesktop() {
           variant="outlined"
           type="password"
           placeholder="Password"
+          required
           sx={{
-            width:500,
-            height:15,
-            marginBottom:5,
+            width: 500,
+            height: 15,
+            marginBottom: 5
           }}
         />
-        <br/>
-        <br/>
+        <br />
+        <br />
 
-        <Button 
-          id="loginBtn" 
-          onClick={onSubmit} 
+        <Button
+          id="loginBtn"
           variant="contained"
+          type='submit'
           sx={{
-            width:500,
-            color:'white',
-            backgroundColor:'#1565c0',
-          }}>Log In</Button>
-        <br/>
-        <br/>
+            width: 500,
+            color: 'white',
+            backgroundColor: '#1565c0'
+          }}>
+          Log In
+        </Button>
+        <br />
+        <br />
 
         <Typography id="signUpDiv">
           Don't have an account? &nbsp;
--- frontend/src/pages/login/index.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import LoginDesktop from './LoginDesktop';
-
-export default function LoginPage() {
-  return React.createElement(LoginDesktop);
-}
